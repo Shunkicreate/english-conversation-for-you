7a46acbf6cb9ef013426653862e05b4c9096efd3
Shunkicreate <you@example.comjmsrsyunrinsyunki@gmail.com>
2022-08-21 23:17:35 +0900

    add chat decorder

---
+++ src/ChatDecoder.tsx
@@ -0,0 +1,18 @@
+import { Chat } from '../types/ChatType'
+export const ChatDecoder = (ChatDatas: string) => {
+    let ReturnData: Chat[] = [];
+    const regex = /\n\n/g
+    ChatDatas = ChatDatas.replace(regex, '\n')
+    let ReturnDataArray: string[] = ChatDatas.split('\n')
+    debugger;
+    for (let i of ReturnDataArray){
+        const devideData: string[] = i.split(':')
+        const singleChatData: Chat = {
+            person: devideData[0],
+            message: devideData[1]
+        }
+        ReturnData.push(singleChatData)
+    }
+    return ReturnData;
+  };
+  
