0871f0c3a67463b94b7e40694295e9d68ca2ad10
Shunkicreate <you@example.comjmsrsyunrinsyunki@gmail.com>
2022-08-21 22:50:46 +0900

    add chat getter

---
+++ src/ChatGetter.tsx
@@ -0,0 +1,12 @@
+import { Chat } from "../types/ChatType";
+export const ChatGetter = (ChatDatas: Chat[]) => {
+  let ReturnData: Chat[] = [];
+  for (let i = 0; i < ChatDatas.length; i++) {
+    if (i > 10) {
+      break;
+    }
+    ReturnData.push(ChatDatas[ChatDatas.length - i]);
+  }
+
+  return ReturnData;
+};
