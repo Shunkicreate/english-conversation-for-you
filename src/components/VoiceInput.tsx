0e2e74781890b3c77fb04c961896f3c6e37babed
Shunkicreate <you@example.comjmsrsyunrinsyunki@gmail.com>
2022-08-22 16:22:29 +0900

    add voice input

---
+++ src/components/VoiceInput.tsx
@@ -0,0 +1,42 @@
+import React, { useState, FC } from "react";
+import { ChatType } from "../../types/ChatType";
+import { InputType } from "../../types/InputType";
+
+export const VoiceInput: FC<InputType> = ({ ChatDatas, setChatDatas,  setDoChat }) => {
+  const [text, setText] = useState("");
+  const handleSubmitEvent = () => {
+    const addData: ChatType = {
+      person: "You",
+      message: text,
+    };
+    setChatDatas([...ChatDatas, addData]);
+    setText("");
+    setDoChat(true)
+  };
+  return (
+    <div className="input">
+      <label>
+        <input
+          type="text"
+          value={text}
+          onChange={(event) => {
+            setText(event.target.value);
+          }}
+          onKeyPress={(event) => {
+            if (event.key === "Enter") {
+              handleSubmitEvent();
+            }
+          }}
+        />
+      </label>
+      <input
+        type="submit"
+        value="Submit"
+        onClick={(event) => {
+          handleSubmitEvent();
+        }}
+      />
+      <div>{text}</div>
+    </div>
+  );
+};
