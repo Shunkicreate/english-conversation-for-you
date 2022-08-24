22069629264015aefcdf9e9557c3d09becb8e01b
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2022-08-24 20:46:07 +0900

    fix chat design

--- src/components/ShowChat.tsx
+++ src/components/ShowChat.tsx
@@ -6,19 +6,15 @@ const OneChat = (person: string, message: string) => {
   if (person === "AI" || person === "OpenAI") {
     return (
       <div className="AI OneChat">
-        <div className="Content">
-          <div className="person">{person}</div>
-          <div className="message">{message}</div>
-        </div>
+        <div className="person">{person}</div>
+        <div className="message">{message}</div>
       </div>
     );
   }
   return (
     <div className="You OneChat">
-      <div className="Content">
-        <div className="person">{person}</div>
-        <div className="message">{message}</div>
-      </div>
+      <div className="person">{person}</div>
+      <div className="message">{message}</div>
     </div>
   );
 };
