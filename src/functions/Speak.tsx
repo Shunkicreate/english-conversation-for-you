fdfb493091b01968fe0080f78a8ad859df2f0b17
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2022-08-25 12:54:03 +0900

    change openapi mode

--- src/functions/Speak.tsx
+++ src/functions/Speak.tsx
@@ -1,12 +1,22 @@
-export const Speak = (SpeacText: string) => {
+export const Speak = (SpeakText: string) => {
   if ("speechSynthesis" in window) {
     // 発言を設定
-    const uttr = new SpeechSynthesisUtterance();
-    uttr.text = SpeacText;
-    uttr.lang = 'en-US';
+    const DevidedSpeakText: string[] = SpeakText.split('. ')
+    console.log(DevidedSpeakText)
+    // const uttr = new SpeechSynthesisUtterance();
+    // uttr.text = SpeakText;
+    // uttr.lang = 'en-US'
+
+
+  //   uttr.onstart = () =>{
+  //     resumeInfinity();
+  //   }
+  //   uttr.onend = function(event) {
+  //     clearTimeout(timeoutResumeInfinity);
+  // };
     
     // 発言を再生
-    window.speechSynthesis.speak(uttr);
+    // window.speechSynthesis.speak(uttr);
   } else {
     alert("Sorry. Your brouser can't speak.");
   }
