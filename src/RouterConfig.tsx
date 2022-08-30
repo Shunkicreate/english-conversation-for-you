d69b790afe3be63e0e95104e8a84985dbb22d6fc
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2022-08-30 17:25:19 +0900

    add watch together paht

--- src/RouterConfig.tsx
+++ src/RouterConfig.tsx
@@ -2,14 +2,16 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import App from "./App";
+import { WatchTogether } from "./components/WatchTogether";
 import { NotFoutnd } from "./components/404";
 export const RouterConfig = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path={`/login`} element={<Login />} />
           <Route path={`/`} element={<App uid={null} />} />
+          <Route path={`/login`} element={<Login />} />
+          <Route path={'/watcht-together'} element={<WatchTogether />} />
           <Route path="*" element={<NotFoutnd />} />
         </Routes>
       </BrowserRouter>
