3f8909b8a468cf811a1149a43bd3299cdf70b3a0
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2022-09-01 14:30:40 +0900

    fix useeffect called two times probram cus this app is setted strict mode

--- src/components/WatchTogether.tsx
+++ src/components/WatchTogether.tsx
@@ -1,8 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { GetVttFile } from "../functions/GetVttFile";
+import axios from 'axios';
+
 export const WatchTogether = () => {
     const [isThumbnail, setIsThumbnail] = useState(true);
     const [VttData, setVttData] = useState("")
+    useEffect(()=>{
+        var data = {
+            "url": "https://www.youtube.com/watch?v=i0ShMDZ_q-U"
+        };
+    
+        var config = {
+            method: 'post',
+            url: 'http://127.0.0.1:5000/youtubeDlSubtitles',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            data: data
+        };
+        axios(config)
+            .then(function (response) {
+                console.log(JSON.stringify(response.data));
+                setVttData(JSON.stringify(response.data))
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    })
     const youtubeId = 'pmG6JxkVzKM';
     return (
         <div className="WatchTogether">
@@ -21,7 +45,9 @@ export const WatchTogether = () => {
                     allowFullScreen
                 ></iframe>
             )}
+            {/* <div>{VttData}</div> */}
         </div>
+        
     );
 
 }
