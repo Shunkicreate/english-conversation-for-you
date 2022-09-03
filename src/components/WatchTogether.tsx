58bc1726efd46e28656fe5db525b2c5fdfcb2be9
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2022-09-03 15:56:51 +0900

    とりあえずyoutube系を繫いだけど動かん

--- src/components/WatchTogether.tsx
+++ src/components/WatchTogether.tsx
@@ -2,13 +2,16 @@ import { useEffect, useState } from "react";
 import { GetVttFile } from "../functions/GetVttFile";
 import { ShowSubtitles } from "./ShowSubtitles";
 import axios from 'axios';
-
+import { GetYouTubeVideoId } from "../functions/GetYouTubeVideoId";
 export const WatchTogether = () => {
     const [isThumbnail, setIsThumbnail] = useState(true);
     const [VttData, setVttData] = useState("")
+    const [InputUrl, setInputUrl] = useState("")
+    const [InputData, setInputData] = useState("")
+    const [YoutubeId, setYoutubeId] = useState("iFg-bFAu2AU")
     useEffect(() => {
         var data = {
-            "url": "https://www.youtube.com/watch?v=iFg-bFAu2AU"
+            "url": InputUrl
         };
 
         var config = {
@@ -24,35 +27,48 @@ export const WatchTogether = () => {
             .then(function (response) {
                 console.log(JSON.stringify(response.data));
                 setVttData(JSON.stringify(response.data))
+                setIsThumbnail(false)
+
             })
             .catch(function (error) {
                 console.log(error);
             });
-    })
-    const youtubeId = 'iFg-bFAu2AU';
+    },[InputUrl])
     return (
         <div className="WatchTogether">
+            <div>
+                <input type="text"
+                    onChange={(e) => {
+                        setInputData(e.target.value)
+                    }}
+                    onKeyDown={(e) => {
+                        if (e.key === 'Enter') {
+                            setInputUrl(InputData)
+                            setYoutubeId(GetYouTubeVideoId(InputUrl))
+                        }
+                    }} />{InputData}{InputUrl}
+            </div>
             {isThumbnail ? (
                 <img
-                    src={`https://img.youtube.com/vi/${youtubeId}/maxresdefault.jpg`}
+                    src={`https://img.youtube.com/vi/${YoutubeId}/maxresdefault.jpg`}
                     onClick={() => setIsThumbnail(false)}
                     alt="サムネイル"
                 />
             ) : (
                 <div>
                     <iframe
-                        src={`https://www.youtube.com/embed/${youtubeId}?autoplay=1`}
+                        src={`https://www.youtube.com/embed/${YoutubeId}?autoplay=1`}
                         title="YouTube video player"
                         frameBorder="0"
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                         allowFullScreen
                     ></iframe>
                     <div>
-                        <video src={`https://www.youtube.com/embed/${youtubeId}?autoplay=1`} title="YouTube video player">
+                        <video src={`https://www.youtube.com/embed/${YoutubeId}?autoplay=1`} title="YouTube video player">
                             <track default src={VttData} />
                         </video>
                     </div>
-                    <ShowSubtitles />
+                    <ShowSubtitles subtitles={VttData} />
                 </div>
             )}
             {/* <div>{VttData}</div> */}
---
+++ src/functions/GetYouTubeVideoId.tsx
@@ -0,0 +1,12 @@
+export const GetYouTubeVideoId = (url: string) => {
+    const name = 'v'
+    let queryString = url.split('?');
+    if (queryString.length >= 2) {
+        let paras = queryString[1].split('&');
+        for (let i = 0; i < paras.length; i++) {
+            let eachPara = paras[i].split('=');
+            if (eachPara[0] === name) return eachPara[1];
+        }
+    }
+    return "";
+}
