71f7ee592b2bfcc34c621a50489967642b9eec7d
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2022-09-05 12:18:15 +0900

    add youtube param

--- src/components/ShowYoutube.tsx
+++ src/components/ShowYoutube.tsx
@@ -1,17 +1,40 @@
 import YouTube from "react-youtube"
 export const ShowYoutube = () => {
+
+    const onPlayerReady = () => {
+        console.log('onPlayerReady')
+    }
+    const onPlayerPlaybackQualityChange = () => {
+        console.log('onPlayerPlaybackQualityChange')
+    }
+    const onPlayerStateChange = () => {
+        console.log('onPlayerStateChange')
+    }
+    const onPlayerError = () => {
+        console.log('onPlayerError')
+    }
     const opts = {
         height: '390',
         width: '640',
         playerVars: { // https://developers.google.com/youtube/player_parameters
-            autoplay: 1
-        }
+            autoplay: 1,
+            cc_load_policy: 0,
+            controls: 0,
+            disablekb: 1,
+            fs: 0,
+            iv_load_policy: 3,
+            modestbranding: 1,
+            playsinline: 1,
+            rel: 0,
+        },
     }
 
     return (
         <YouTube
-            videoId="2g811Eo7K8U"
+            videoId="1eFq21yT1vE"
             opts={opts}
+            onStateChange={(e) => {onPlayerStateChange()}}
+
         />
     )
 
