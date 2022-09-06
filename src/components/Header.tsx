74e82a8f16a3985835dd080f6678e43a02e663af
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2022-09-06 17:51:06 +0900

    add route to watch together

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,12 @@
 import '../stylesheets/Header.css'
+import { useNavigate } from 'react-router-dom';
 export const Header = () => {
+  const navigate = useNavigate();
+
   return (
     <header>
       <div className="Title">AI Chat</div>
+      <div className='button' onClick={() => {navigate('/watch-together')}}>watch YouTube together</div>
       <div className='Login'>login</div>
     </header>
   )
