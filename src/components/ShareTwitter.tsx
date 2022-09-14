ecb9f0d693cc07ec03377b01898fc09f24c36547
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2022-09-14 10:28:52 +0900

    add tweet button

--- src/components/ShareTwitter.tsx
+++ src/components/ShareTwitter.tsx
@@ -1,8 +1,9 @@
+import "../stylesheets/ShareTwitter.css"
 export const ShareTwitter = () => {
     return (
-        
-        <div>
-            <a href="https://twitter.com/intent/tweet?text=Hello World&url=https://merry-torte-fbd589.netlify.app/&hashtags=react,typeScript">Share Twitter</a>
+
+        <div id="widget">
+            <div className="btn-o" data-scribe="component:button"><a href="https://twitter.com/intent/tweet?original_referer=https%3A%2F%2Fdeveloper.twitter.com%2F&amp;ref_src=twsrc%5Etfw%7Ctwcamp%5Ebuttonembed%7Ctwterm%5Eshare%7Ctwgr%5E&amp;text=Guides%20%7C%20Docs%20%7C%20Twitter%20Developer%20Platform&amp;url=https%3A%2F%2Fdeveloper.twitter.com%2Fen%2Fdocs%2Ftwitter-for-websites%2Ftweet-button%2Foverview" className="btn" id="b" target="_blank" rel="noopener noreferrer" ><i></i><span className="label" id="l">Tweet</span></a></div>
         </div>
     )
 }
