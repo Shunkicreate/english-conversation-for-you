d1c97f78366b0fdcd139721aa9e06a3ca057caed
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2022-09-14 10:53:14 +0900

    fix share design

--- src/components/ShareTwitter.tsx
+++ src/components/ShareTwitter.tsx
@@ -3,7 +3,7 @@ export const ShareTwitter = () => {
     return (
 
         <div id="widget">
-            <div className="btn-o" data-scribe="component:button"><a href="https://twitter.com/intent/tweet?original_referer=https%3A%2F%2Fdeveloper.twitter.com%2F&amp;ref_src=twsrc%5Etfw%7Ctwcamp%5Ebuttonembed%7Ctwterm%5Eshare%7Ctwgr%5E&amp;text=Guides%20%7C%20Docs%20%7C%20Twitter%20Developer%20Platform&amp;url=https%3A%2F%2Fdeveloper.twitter.com%2Fen%2Fdocs%2Ftwitter-for-websites%2Ftweet-button%2Foverview" className="btn" id="b" target="_blank" rel="noopener noreferrer" ><i></i><span className="label" id="l">Tweet</span></a></div>
+            <div className="btn-o" data-scribe="component:button"><a href="https://twitter.com/intent/tweet?original_referer=https%3A%2F%2Fdeveloper.twitter.com%2F&amp;ref_src=twsrc%5Etfw%7Ctwcamp%5Ebuttonembed%7Ctwterm%5Eshare%7Ctwgr%5E&amp;text=Guides%20%7C%20Docs%20%7C%20Twitter%20Developer%20Platform&amp;url=https%3A%2F%2Fdeveloper.twitter.com%2Fen%2Fdocs%2Ftwitter-for-websites%2Ftweet-button%2Foverview" className="btn" id="b" target="_blank" rel="noopener noreferrer" ><i></i><span className="label" id="l">Share Chat in Twitter</span></a></div>
         </div>
     )
 }
